b753e8b56b49107f364f269080838d0bf0c61ab1
hemnathworld <40796463+hemnathworld@users.noreply.github.com>
2024-10-21 07:17:37 +0530

    Update hadr.tf

--- hadr.tf
+++ hadr.tf
@@ -1,64 +1,54 @@
-# Read config from JSON file
-variable "config" {
-  type = object({
-    region               = string
-    bucket_name          = string
-    eb_app_name          = string
-    env_name             = string
-    instance_type        = string
-    solution_stack       = string
-    route53_zone_id      = string
-    domain_name          = string
-    zone_id              = string
-    health_check_enabled = bool
-  })
-}
-
 # AWS provider for the given region
 provider "aws" {
-  region = var.config.region
+  region = var.region
 }
 
-# S3 bucket for Elastic Beanstalk app versions
-resource "aws_s3_bucket" "eb_application_bucket" {
-  bucket = var.config.bucket_name
+# Conditional creation of Private DNS Hosted Zone (only in us-west-1)
+resource "aws_route53_zone" "private_dns_zone" {
+  count       = var.region == "us-west-1" ? 1 : 0
+  name        = var.private_dns_domain_name
+  vpc {
+    vpc_id = var.vpc_id
+  }
+  comment     = "Private hosted zone for internal DNS"
+  private_zone = true
 }
 
 # Elastic Beanstalk Application
 resource "aws_elastic_beanstalk_application" "app" {
-  name        = var.config.eb_app_name
+  name        = var.eb_app_name
   description = "Elastic Beanstalk application"
 }
 
 # Elastic Beanstalk environment
 resource "aws_elastic_beanstalk_environment" "env" {
-  name                = var.config.env_name
+  name                = var.env_name
   application         = aws_elastic_beanstalk_application.app.name
-  solution_stack_name = var.config.solution_stack
+  solution_stack_name = var.solution_stack
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "InstanceType"
-    value     = var.config.instance_type
+    value     = var.instance_type
   }
 }
 
-# Route 53 DNS record for the environment
-resource "aws_route53_record" "eb_dns" {
-  zone_id = var.config.route53_zone_id
-  name    = var.config.domain_name
+# Route 53 DNS record for the environment (Private DNS)
+resource "aws_route53_record" "eb_private_dns" {
+  zone_id = coalesce(aws_route53_zone.private_dns_zone[0].id, var.existing_private_zone_id)
+  name    = var.private_dns_record
   type    = "A"
 
   alias {
     name                   = aws_elastic_beanstalk_environment.env.endpoint_url
-    zone_id                = var.config.zone_id
-    evaluate_target_health = var.config.health_check_enabled
+    zone_id                = var.zone_id
+    evaluate_target_health = var.health_check_enabled
   }
 }
 
-# (Optional) Route 53 health check if enabled
+# Optional Route 53 health check if enabled
 resource "aws_route53_health_check" "eb_health_check" {
-  count              = var.config.health_check_enabled ? 1 : 0
+  count              = var.health_check_enabled ? 1 : 0
   fqdn               = aws_elastic_beanstalk_environment.env.endpoint_url
   type               = "HTTPS"
   resource_path      = "/"
