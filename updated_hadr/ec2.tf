ff7b8d3134f90be353f08699a270f5060bf730b3
hemnathworld <40796463+hemnathworld@users.noreply.github.com>
2024-10-28 08:16:53 +0530

    Update ec2.tf

--- updated_hadr/ec2.tf
+++ updated_hadr/ec2.tf
@@ -8,9 +8,10 @@ locals {
   user_data_file = lookup(local.user_data_files, var.region)
 }
 
-resource "aws_security_group" "allow_http" {
+resource "aws_security_group" "allow_http_primary" {
+  count    = var.region == "us-west-1" ? 1 : 0
   name_prefix = "allow_http"
-
+  vpc_id = var.primary_vpc_id
   ingress {
     from_port   = 80
     to_port     = 80
@@ -33,11 +34,38 @@ resource "aws_security_group" "allow_http" {
   }
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_security_group" "allow_http_secondary" {
+  count    = var.region == "us-east-1" ? 1 : 0
+  name_prefix = "allow_http"
+  vpc_id = var.secondary_vpc_id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "app_server_primary" {
+  count    = var.region == "us-west-1" ? 1 : 0
   ami                    = var.ami
  instance_type          = "t2.micro"
   iam_instance_profile   = var.iam_profile_name
-  vpc_security_group_ids = [aws_security_group.allow_http.id]
+  vpc_security_group_ids = [aws_security_group.allow_http[count.index].id]
   subnet_id              = var.subnet_id
   private_ip             = var.private_ip
 
@@ -50,3 +78,23 @@ resource "aws_instance" "app_server" {
     Name = "AppServer"
   }
 }
+
+resource "aws_instance" "app_server" {
+  count    = var.region == "us-east-1" ? 1 : 0
+  ami                    = var.ami
+ instance_type          = "t2.micro"
+  iam_instance_profile   = var.iam_profile_name
+  vpc_security_group_ids = [aws_security_group.allow_http_secondary[count.index].id]
+  subnet_id              = var.subnet_id
+  private_ip             = var.private_ip
+
+  user_data = <<-EOF
+    #!/bin/bash
+    ${file(local.user_data_file)}
+  EOF
+
+  tags = {
+    Name = "AppServer"
+  }
+}
+
