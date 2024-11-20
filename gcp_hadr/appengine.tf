ed49d065009fc7d0b09b0fdd2d8d4a5008dcf077
hemnathworld <40796463+hemnathworld@users.noreply.github.com>
2024-11-20 22:47:31 +0530

    Update appengine.tf

--- gcp_hadr/appengine.tf
+++ gcp_hadr/appengine.tf
@@ -11,23 +11,19 @@ resource "google_project_iam_member" "app_engine_sa_storage_access" {
   member  = "serviceAccount:${google_service_account.app_engine_sa.email}"
 }
 
-resource "google_storage_bucket_iam_member" "cloud_build_access" {
-  bucket = "staging.${var.project_id}.appspot.com"
-  role   = "roles/storage.objectViewer"
-  member = "serviceAccount:${var.project_id}@appspot.gserviceaccount.com"
-}
-
-# Create App Engine in each region
-resource "google_app_engine_application" "app" {
-  location_id = var.region
-}
-
-
 # Upload app source code to Cloud Storage
-resource "google_storage_bucket_object" "app_code" {
-  name   = "sample-app.zip"
+resource "google_storage_bucket_object" "app_code_primary" {
+  count    = var.region == "us-west1" ? 1 : 0
+  name   = "sample-app-us-west1.zip"
   bucket = google_storage_bucket.primary_bucket.name
-  source = "${path.module}/sample-app.zip"
+  source = "${path.module}/sample-app-us-west1.zip"
+}
+
+resource "google_storage_bucket_object" "app_code_secondary" {
+  count    = var.region == "us-east1" ? 1 : 0
+  name   = "sample-app-us-east1.zip"
+  bucket = google_storage_bucket.primary_bucket.name
+  source = "${path.module}/sample-app-us-east1.zip"
 }
 
 resource "google_app_engine_standard_app_version" "app_version_primary" {
@@ -36,12 +32,12 @@ resource "google_app_engine_standard_app_version" "app_version_primary" {
   version_id = "v1"
   runtime   = "python39"
   entrypoint {
-    shell = "python main_us-west1.py"
+    shell = "gunicorn -w 2 -b :8080 main:app"
   }
 
   deployment {
     zip {
-      source_url = "https://storage.googleapis.com/hemtestbucket-hadr-us-west1/sample-app.zip"
+      source_url = "https://storage.googleapis.com/hemtestbucket-hadr-us-west1/sample-app-us-west1.zip"
     }
   }
   service_account = google_service_account.app_engine_sa.email
@@ -53,12 +49,12 @@ resource "google_app_engine_standard_app_version" "app_version_secondary" {
   version_id = "v1"
   runtime   = "python39"
   entrypoint {
-    shell = "python main_us-east1.py"
+    shell = "gunicorn -w 2 -b :8080 main:app"
   }
 
-  deployment {
+  deployment { 
     zip {
-      source_url = "https://storage.googleapis.com/hemtestbucket-hadr-us-west1/sample-app.zip"
+      source_url = "https://storage.googleapis.com/hemtestbucket-hadr-us-east1/sample-app-us-east1.zip"
     }
   }
   service_account = google_service_account.app_engine_sa.email
