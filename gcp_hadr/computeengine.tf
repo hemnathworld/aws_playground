04f4ed66c951eed80cb682e243bb95fbbefeb1ec
hemnathworld <40796463+hemnathworld@users.noreply.github.com>
2024-11-28 08:03:30 +0530

    Update computeengine.tf

--- gcp_hadr/computeengine.tf
+++ gcp_hadr/computeengine.tf
@@ -1,74 +1,40 @@
-resource "google_compute_instance_template" "template-west1" {
-  count    = var.region == "us-west1" ? 1 : 0
+resource "google_compute_instance" "vm_instance_primary" {
+  count    = var.region == "us-west4" ? 1 : 0
   provider     = google.hub
-  name        = "instance-template-west1"
+  name         = "gcp-hadr-web-primary"
   machine_type = "e2-micro"
-  network_interface {
-    network    = var.vpc_id
-    subnetwork = var.subnet_id
-    access_config {
+  zone         = "us-west4-b"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
     }
   }
 
-  tags = ["http-server", "https-server"]
-  disk {
-    source_image      = "debian-cloud/debian-11"
-    auto_delete       = true
-    boot              = true
-  }
-  metadata = {
-    startup-script = templatefile("${path.module}/scripts/us-west1-script.sh", {PROJECT_ID = var.project_id})
-  }
-
-}
-
-resource "google_compute_instance_group_manager" "group-west1" {
-  count    = var.region == "us-west1" ? 1 : 0
-  provider     = google.hub
-  name               = "instance-group-west1"
-  base_instance_name = "instance-west1"
-  zone               = "us-west1-a"
-  target_size        = 1
-  version {
-    instance_template = google_compute_instance_template.template-west1[count.index].self_link
-    name              = "primary"
-  }
-}
-
-
-resource "google_compute_instance_template" "template-east1" {
-  count    = var.region == "us-east1" ? 1 : 0
-  provider     = google.hub
-  name        = "instance-template-east1"
-  machine_type = "e2-micro"
   network_interface {
-    network    = var.vpc_id
-    subnetwork = var.subnet_id
-    access_config {
+    network = var.vpc_id
+    access_config {}
+  }
+  metadata_startup_script = templatefile("${path.module}/scripts/us-west1-script.sh", {PROJECT_ID = var.project_id})
+}
+
+
+resource "google_compute_instance" "vm_instance_secondary" {
+  count    = var.region == "us-east4" ? 1 : 0
+  provider     = google.hub
+  name         = "gcp-hadr-web-primary"
+  machine_type = "e2-micro"
+  zone         = "us-east4-b"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
     }
   }
 
-  tags = ["http-server", "https-server"]
-  disk {
-    source_image      = "debian-cloud/debian-11"
-    auto_delete       = true
-    boot              = true
+  network_interface {
+    network = var.vpc_id
+    access_config {}
   }
-  metadata = {
-    startup-script = templatefile("${path.module}/scripts/us-east1-script.sh", {PROJECT_ID = var.project_id})
-  }
-
-}
-
-resource "google_compute_instance_group_manager" "group-east1" {
-  count    = var.region == "us-east1" ? 1 : 0
-  provider     = google.hub
-  name               = "instance-group-east1"
-  base_instance_name = "instance-east1"
-  zone               = "us-east1-b"
-  target_size        = 1
-  version {
-    instance_template = google_compute_instance_template.template-east1[count.index].self_link
-    name              = "primary"
-  }
+  metadata_startup_script = templatefile("${path.module}/scripts/us-east1-script.sh", {PROJECT_ID = var.project_id})
 }
