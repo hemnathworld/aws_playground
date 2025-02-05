26317c2f235cbba29cdd690eba060e0c7fb2db38
hemnathworld <40796463+hemnathworld@users.noreply.github.com>
2025-02-05 06:56:44 +0530

    Update variables.tf

--- gcp_hadr_new/variables.tf
+++ gcp_hadr_new/variables.tf
@@ -3,19 +3,23 @@ variable "region" {
   type        = string
 }
 
-variable "bucket_name" {
-  description = "Name of the Cloud Storage S3 bucket"
+variable "source_bucket_name" {
+  description = "Name of the Cloud Storage primary bucket"
   type        = string
 }
 
-variable "project_id" {
-  type = string
+variable "target_bucket_name" {
+  description = "Name of the Cloud Storage secondary bucket"
+  type        = string
 }
 
 variable "source_project_id" {
   type = string
 }
 
+variable "target_project_id" {
+  type = string
+}
 
 variable "topic_name" {
   description = "The name of the Pub/Sub topic"
@@ -27,13 +31,7 @@ variable "subscriber_name" {
   type        = string
 }
 
-variable "secondary_project_number" {
+variable "target_project_number" {
   description = "Project Number of the secondary region"
   type        = number
-  default = "303082797220"
-}
-
-variable "source_bucket_name" {
-  description = "Bucket Number of the primary region"
-  type        = string
 }
