e97389ed9ff8d943932defcce82a3e719415fadb
hemnathworld <40796463+hemnathworld@users.noreply.github.com>
2025-02-06 07:58:11 +0530

    Update cloudsql.tf

--- gcp_hadr_new/cloudsql.tf
+++ gcp_hadr_new/cloudsql.tf
@@ -8,6 +8,7 @@ resource "google_compute_global_address" "primary_private_ip_range" {
 }
 
 resource "google_service_networking_connection" "primary_private_vpc_connection" {
+  count    = var.region == "us-west4" ? 1 : 0
   network       = <>
   service       = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.primary_private_ip_range.name]
