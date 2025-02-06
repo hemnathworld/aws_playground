9b4a8a3383b49cf4bda3fd4f59db78a1f487b5c5
hemnathworld <40796463+hemnathworld@users.noreply.github.com>
2025-02-06 07:21:42 +0530

    Update cloudsql.tf

--- gcp_hadr_new/cloudsql.tf
+++ gcp_hadr_new/cloudsql.tf
@@ -1,5 +1,6 @@
-resource "google_sql_database_instance" "sql_instance" {
-  name             = "db-instance"
+resource "google_sql_database_instance" "primary_sql_instance" {
+  count    = var.region == "us-west4" ? 1 : 0
+  name             = "primary-db-instance"
   database_version = "MYSQL_8_0"
   region           = var.region
 
@@ -14,6 +15,30 @@ resource "google_sql_database_instance" "sql_instance" {
 
 # Cloud SQL Database
 resource "google_sql_database" "database" {
-  name     = "mydatabase"
+  count    = var.region == "us-west4" ? 1 : 0
+  name     = "myprimarydatabase"
   instance = google_sql_database_instance.sql_instance.name
 }
+
+resource "google_sql_database_instance" "sql_instance" {
+  count    = var.region == "us-east4" ? 1 : 0
+  name             = "secondary-db-instance"
+  database_version = "MYSQL_8_0"
+  region           = var.region
+
+  settings {
+    tier = "db-f1-micro" # Change to appropriate machine type
+    backup_configuration {
+      enabled            = true
+      point_in_time_recovery_enabled = true
+    }
+  }
+}
+
+# Cloud SQL Database
+resource "google_sql_database" "database" {
+  count    = var.region == "us-west4" ? 1 : 0
+  name     = "myprimarydatabase"
+  instance = google_sql_database_instance.sql_instance.name
+}
+
