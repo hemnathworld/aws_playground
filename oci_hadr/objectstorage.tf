c34493ff25e3e8c895c712e28da8e4203e14085f
hemnathworld <40796463+hemnathworld@users.noreply.github.com>
2025-02-20 07:50:51 +0530

    Create objectstorage.tf

---
+++ oci_hadr/objectstorage.tf
@@ -0,0 +1,21 @@
+
+data "oci_objectstorage_namespace" "ns" {}
+
+resource "oci_objectstorage_bucket" "source_bucket" {
+  count    = var.region == "us-luke-1" ? 1 : 0  ## West
+  compartment_id = var.compartment_id
+  name           = var.source_bucket_name
+  namespace      = data.oci_objectstorage_namespace.ns.namespace
+  region         = var.region
+  storage_tier   = "Standard"
+}
+
+resource "oci_objectstorage_bucket" "target_bucket" {
+  count    = var.region == "us-langley-1" ? 1 : 0  ## East 
+  provider       = oci.target_region
+  compartment_id = var.compartment_id
+  name           = var.target_bucket_name
+  namespace      = data.oci_objectstorage_namespace.ns.namespace
+  region         = var.region
+  storage_tier   = "Standard"
+}
