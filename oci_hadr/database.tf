a9dbb7fb1dab4796e5e118eac9ce622ba417b0b2
hemnathworld <40796463+hemnathworld@users.noreply.github.com>
2025-02-21 07:40:16 +0530

    Update database.tf

--- oci_hadr/database.tf
+++ oci_hadr/database.tf
@@ -1,11 +1,13 @@
-resource "tls_private_key" "db_ssh_key" {
+resource "tls_private_key" "source_db_ssh_key" {
+  count    = var.region == "us-luke-1" ? 1 : 0  ## West
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "local_file" "private_key" {
-  content  = tls_private_key.db_ssh_key.private_key_pem
-  filename = "${path.module}/db_private_key.pem"
+resource "local_file" "source_private_key" {
+  count    = var.region == "us-luke-1" ? 1 : 0  ## West
+  content  = tls_private_key.source_db_ssh_key.private_key_pem
+  filename = "${path.module}/source_db_private_key.pem"
   file_permission = "0600"
 }
 
@@ -20,7 +22,7 @@ resource "oci_database_db_system" "source_database" {
   database_edition     = "ENTERPRISE_EDITION"
   node_count           = 1
   license_model        = "LICENSE_INCLUDED"
-  ssh_public_keys      = [tls_private_key.db_ssh_key.public_key_openssh]
+  ssh_public_keys      = [tls_private_key.source_db_ssh_key.public_key_openssh]
 
   db_home {
     display_name     = "testDBHome"
