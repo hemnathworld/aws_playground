9d3bb1821f7ab047600d4d0885d45df8181bbd3e
hemnathworld <40796463+hemnathworld@users.noreply.github.com>
2025-03-01 16:21:37 +0530

    Update us_west.sh

--- oci_hadr/scripts/us_west.sh
+++ oci_hadr/scripts/us_west.sh
@@ -4,4 +4,4 @@ yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
 
-echo "<h1>Welcome to Web Server in US GOV WEST 1</h1>" > /var/www/html/index.html
+echo "<h1>Welcome to Web Server in US GOV WEST </h1>" > /var/www/html/index.html
