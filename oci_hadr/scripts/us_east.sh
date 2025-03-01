66fa7482b5cb01b814910525572d345cf6aa01a6
hemnathworld <40796463+hemnathworld@users.noreply.github.com>
2025-03-01 16:21:25 +0530

    Update us_east.sh

--- oci_hadr/scripts/us_east.sh
+++ oci_hadr/scripts/us_east.sh
@@ -4,4 +4,4 @@ yum install -y httpd
 systemctl start httpd
 systemctl enable httpd
 
-echo "<h1>Welcome to Web Server in US-LANGLEY-1</h1>" > /var/www/html/index.html
+echo "<h1>Welcome to Web Server in US GOV EAST </h1>" > /var/www/html/index.html
