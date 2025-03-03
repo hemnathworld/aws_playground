be3b9669c7469d858f77b332cd695a28241b7efc
hemnathworld <40796463+hemnathworld@users.noreply.github.com>
2025-03-03 22:31:31 +0530

    Update instance.tf

--- oci_hadr/instance.tf
+++ oci_hadr/instance.tf
@@ -1,3 +1,8 @@
+data "oci_core_subnet" "primary_external_subnet" {
+  compartment_id = var.connectivity_compartment_id  # Use a connectivity compartment
+  subnet_id      = var.subnet_id       # Pass the OCID of the connectivity subnet
+}
+
 resource "oci_core_instance" "primary_vm" {
   count               = var.region == "us-luke-1" ? 1 : 0
   compartment_id      = var.compartment_id
@@ -5,7 +10,7 @@ resource "oci_core_instance" "primary_vm" {
   shape              = var.shape
 
   create_vnic_details {
-    subnet_id = var.subnet_id
+    subnet_id = data.oci_core_subnet.external_subnet.id  # Fetching from another compartment
   }
 
   source_details {
