ecb55ffeacd0894ae701afdc680547c6c5d1d862
hemnathworld <40796463+hemnathworld@users.noreply.github.com>
2025-04-04 06:53:32 +0530

    Update policies.tf

--- azure/policies.tf
+++ azure/policies.tf
@@ -9,10 +9,17 @@ data "azurerm_management_group" "my_management_group" {
   name = var.env
 }
 
+locals {
+  short_policy_names = {
+    for name in var.policy_names :
+    name => replace(lower(join("-", slice(split(" ", name), 0, 4))), "/[^a-z0-9-/", "")
+  }
+}
+
 # Policy Assignment to enforce the deployment of Azure Monitor Agent at the management group level
 resource "azurerm_policy_assignment" "monitor_agent_assignment" {
   for_each = data.azurerm_policy_definition.policy_definitions
-  name                 = "assignment-${each.key}"
+  name                 = "asg-${local.short_policy_names[each.key]}"
   scope                = data.azurerm_management_group.my_management_group.id
   policy_definition_id = each.value.id
   display_name         = "Deploy Azure Monitor Agent on VMs"
