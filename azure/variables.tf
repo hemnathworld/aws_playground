c433a16e2e797bfe9cd3472f0dfea321e20483ce
hemnathworld <40796463+hemnathworld@users.noreply.github.com>
2025-04-04 08:22:39 +0530

    Update variables.tf

--- azure/variables.tf
+++ azure/variables.tf
@@ -11,29 +11,29 @@ variable "policy_names" {
 variable "policy_parameters" {
   type = map(map(any))
   default = {
-    "[Preview]: Assign Built-In User-Assigned Managed Identity to Virtual Machines" = {
+    "[Preview]: Assign Built-In User-Assigned Managed Identity to Virtual Machines" = jsonencode({
       "bringYourOwnUserAssignedManagedIdentity" = false
-    }
-    "[Preview]: Assign Built-In User-Assigned Managed Identity to Virtual Machine Scale Sets" = {
+    })
+    "[Preview]: Assign Built-In User-Assigned Managed Identity to Virtual Machine Scale Sets" = jsonencode({
       "bringYourOwnUserAssignedManagedIdentity" = false
-    }
-    "Configure Windows virtual machines to run Azure Monitor Agent with user-assigned managed identity-based authentication" = {
+    })
+    "Configure Windows virtual machines to run Azure Monitor Agent with user-assigned managed identity-based authentication" = jsonencode({
       "bringYourOwnUserAssignedManagedIdentity" = false
-    }
-    "Configure Windows virtual machine scale sets to run Azure Monitor Agent with user-assigned managed identity-based authentication" = {
+    })
+    "Configure Windows virtual machine scale sets to run Azure Monitor Agent with user-assigned managed identity-based authentication" = jsonencode({
       "bringYourOwnUserAssignedManagedIdentity" = false
-    }
-    "Configure Windows Machines to be associated with a Data Collection Rule or a Data Collection Endpoint" = {
+    })
+    "Configure Windows Machines to be associated with a Data Collection Rule or a Data Collection Endpoint" = jsonencode({
       "dcrResourceId"     = "/subscriptions/xxxx/resourceGroups/rg-name/providers/Microsoft.Insights/dataCollectionRules/dcr-nam"
-    }
-    "Configure Linux virtual machines to run Azure Monitor Agent with user-assigned managed identity-based authentication" = {
+    })
+    "Configure Linux virtual machines to run Azure Monitor Agent with user-assigned managed identity-based authentication" = jsonencode({
        "bringYourOwnUserAssignedManagedIdentity" = false
-    }
-    "Configure Linux virtual machine scale sets to run Azure Monitor Agent with user-assigned managed identity-based authentication" = {
+    })
+    "Configure Linux virtual machine scale sets to run Azure Monitor Agent with user-assigned managed identity-based authentication" = jsonencode({
        "bringYourOwnUserAssignedManagedIdentity" = false
-    }
-    "Configure Linux Machines to be associated with a Data Collection Rule or a Data Collection Endpoint" = {
+    })
+    "Configure Linux Machines to be associated with a Data Collection Rule or a Data Collection Endpoint" = jsonencode({
       "dcrResourceId"     = "/subscriptions/xxxx/resourceGroups/rg-name/providers/Microsoft.Insights/dataCollectionRules/dcr-nam"
-    }
+    })
   }
 }
