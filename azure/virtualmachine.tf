e1257b79bc8e5bf7279a49ffaf62d613a5584d3c
hemnathworld <40796463+hemnathworld@users.noreply.github.com>
2025-04-16 06:54:31 +0530

    Update virtualmachine.tf

--- azure/virtualmachine.tf
+++ azure/virtualmachine.tf
@@ -1,38 +1,3 @@
-resource "azurerm_network_interface" "nic" {
-  name                = "${var.vm_name}-nic"
-  location            = var.location
-  resource_group_name = var.resource_group_name
-
-  ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = var.subnet_id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_windows_virtual_machine" "vm" {
-  name                  = var.vm_name
-  resource_group_name   = var.resource_group_name
-  location              = var.location
-  size                  = "Standard_B1s"
-  admin_username        = var.admin_username
-  admin_password        = var.admin_password
-  network_interface_ids = [azurerm_network_interface.nic.id]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-    name                 = "${var.vm_name}-osdisk"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
-    version   = "latest"
-  }
-}
-
 resource "azurerm_virtual_machine_extension" "ama" {
  name                       = "testextension"
  virtual_machine_id         = azurerm_windows_virtual_machine.vm.id
@@ -78,7 +43,7 @@ resource "azurerm_monitor_data_collection_rule" "rule1" {
 # data collection rule association
  
 resource "azurerm_monitor_data_collection_rule_association" "dcra1" {
- name                    = "testvm-dcra"
+ name                    = "${var.vm_name}-dcra"
  target_resource_id      = azurerm_windows_virtual_machine.vm.id
  data_collection_rule_id = azurerm_monitor_data_collection_rule.rule1.id
 }
